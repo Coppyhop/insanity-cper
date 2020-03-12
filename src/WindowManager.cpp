f09f6b563e4319c9960b9b4533d21fcf78b77db8
Coppy Bredenkamp <kjbredenkamp@email.arizona.edu>
2020-03-11 17:53:38 -0700

    Forgot to add the source files >.>

---
+++ src/WindowManager.cpp
@@ -0,0 +1,42 @@
+#undef GLFW_DLL
+#include <GLFW/glfw3.h>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include "engine.h"
+
+WindowManager::WindowManager(){
+    std::cout<<"Window manager insantiated"<<std::endl;
+}
+
+void WindowManager::init(){
+    //glfwSetErrorCallback(glfwerror_callback);
+    if(!glfwInit()){
+        std::cerr << "Unable to Initialize GLFW" << std::endl;
+    }
+}
+
+GLFWwindow* WindowManager::createWindow(int width, int height, 
+        const char * title, bool vSync){
+    glfwDefaultWindowHints();
+    glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    glfwWindowHint(GLFW_DECORATED, GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+}
+
+bool WindowManager::shouldWindowClose(GLFWwindow* window){
+    return glfwWindowShouldClose(window);
+}
+
+void WindowManager::update(GLFWwindow* window){
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+void WindowManager::destroyWindow(GLFWwindow* window){
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
